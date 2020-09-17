f88d139e8257c5f76c28674a82f3ccb38035d1ba
Mattias Persson <mattias.persson.87@gmail.com>
2020-09-17 19:38:45 +0200

    update

--- custom_components/hpprinter/api/__init__.py
+++
@@ -1,7 +0,0 @@
-class LoginError(Exception):
-    def __init__(self, status_code):
-        self._status_code = status_code
-
-    @property
-    def status_code(self):
-        return self._status_code
