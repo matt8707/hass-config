db57ac6b8bff359c936a914a67d0a2de431abe0b
Mattias Persson <mattias.persson.87@gmail.com>
2020-10-17 21:01:36 +0200

    update

---
+++ www/community/kiosk-mode/kiosk-mode.js
@@ -0,0 +1,29 @@
+if (window.location.href.indexOf('kiosk') > 0) {
+    setTimeout(function() {
+        try {
+            const home_assistant_main = document
+                .querySelector("body > home-assistant").shadowRoot
+                .querySelector("home-assistant-main");
+
+            const header = home_assistant_main.shadowRoot
+                .querySelector("app-drawer-layout > partial-panel-resolver > ha-panel-lovelace").shadowRoot
+                .querySelector("hui-root").shadowRoot
+                .querySelector("#layout > app-header")
+                .style.display = "none";
+
+            const drawer = home_assistant_main.shadowRoot
+                .querySelector("#drawer")
+                .style.display = 'none';
+
+            home_assistant_main.style.setProperty("--app-drawer-width", 0);
+            home_assistant_main.shadowRoot
+                .querySelector("#drawer > ha-sidebar").shadowRoot
+                .querySelector("div.menu")
+                .click();
+            window.dispatchEvent(new Event('resize'));
+        } 
+        catch (e) {
+            console.log(e);
+        }
+    }, 200);
+}
