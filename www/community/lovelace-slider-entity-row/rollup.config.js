29230d01567bff9586cbf2c587836c8603e5370e
Mattias Persson <mattias.persson.87@gmail.com>
2021-03-04 22:24:17 +0100

    update custom cards

---
+++ www/community/lovelace-slider-entity-row/rollup.config.js
@@ -0,0 +1,24 @@
+import nodeResolve from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+import babel from "@rollup/plugin-babel";
+
+const dev = process.env.ROLLUP_WATCH;
+
+export default {
+  input: "src/main.ts",
+  output: {
+    file: "slider-entity-row.js",
+    format: "es",
+  },
+  plugins: [
+    nodeResolve(),
+    json(),
+    typescript(),
+    babel({
+      exclude: "node_modules/**",
+    }),
+    !dev && terser({ format: { comments: false } }),
+  ],
+};
